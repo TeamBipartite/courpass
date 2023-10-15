597b2058820d0ab87a2064fc1fa992176c24a304
ecamartins <65371150+ecamartins@users.noreply.github.com>
2023-10-15 09:32:46 -0700

    chore: mock scraped data

--- academic_calendar_scraper/course_planning_tool/views.py
+++ academic_calendar_scraper/course_planning_tool/views.py
@@ -7,9 +7,12 @@ def course_planning_tool(request):
     c = {}
     c.update(csrf(request))
 
+    courses = ["CSC225", "MATH222"]
+
     if request.method == "POST":
         c['searched'] = request.POST["prereq-search"]
         c['course'] = request.POST["course"]
+        c['course_list'] = courses
 
         return render(request, "course_planning_tool_ui.html", c)
     else:
