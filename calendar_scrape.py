b2686c2c5e3326338fc64f94f8c009db5d7287bd
Johnathan Warawa <johnnyw@johnnyw.ca>
2023-10-15 09:23:11 -0700

    WEBSCRAPE_0013 Cleanup of special cases for calendar scraper

--- calendar_scrape.py
+++ calendar_scrape.py
@@ -7,13 +7,11 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from bs4 import BeautifulSoup
 import pprint
 from collections import namedtuple
+import re
 
 '''
 numReqd is an integer representing the number of items from the reqs_list are 
-        required, where:
-        - 0 represents 'ALL'
-        - -1 is used to indidate a single course entry itself
-        - -2 is used to indicate 'or permission from the department'  
+        required. See constants below for more details and pecial cases.
 reqs_list is a list of PrereqTrees representing the items that are required. If
           num_reqd == -1, then this is a single course
 min_grade is a string representing a minimum grade needed, as a letter grade
@@ -22,10 +20,22 @@ notes is a string giving space for any other special conditions on the req
 '''          
 PrereqTree = namedtuple('PrereqTree', ['num_reqd', 'reqs_list', 'min_grade', 'notes'])
 
+# Constants for num_reqd field
+ALL           =  0 # All subtrees in reqs_list
+SINGLE_COURSE = -1 # Reqs list is a single course and not a PrereqTree, (ie,
+                   # leaf of the tree
+DEPMT_PERMSN  = -2 # 'permission from the deparment' option. Empty reqs_list
+AWR           = -3 # Academic Writing Requirement. Empty reqs_list
+MIN_YR_STNDG  = -4 # Minimum year standing required. Year specified as an int by
+                   # min_grade field
+
 WEBDRIVERS  = {webdriver.ChromeOptions: webdriver.Chrome, 
                webdriver.EdgeOptions: webdriver.Edge,
                webdriver.SafariOptions: webdriver.Safari,
                webdriver.FirefoxOptions: webdriver.Firefox}
+
+
+WORD_TO_RANK = {'first': 1, 'second': 2, 'third': 3, 'fourth': 4, 'fifth': 5}
 
 def get_calendar_info(url) -> list[str]:
     driver = None
@@ -92,11 +102,12 @@ def parse_reqs_rec(reqs_tree: BeautifulSoup) -> PrereqTree:
 
     # special cases that do not follow any other format
     if req_title == 'or permission of the department.': 
-        return PrereqTree(-2, None, None, None)
+        return PrereqTree(DEPMT_PERMSN, [], None, None)
     elif req_title == 'Academic Writing Requirement (AWR) satisfied':
-        return PrereqTree(0, ['AWR'], None, None)
+        return PrereqTree(AWR, [], None, None)
     elif '-year standing' in req_title: # for 'minimum xyz-year standing'
-        return PrereqTree(0, None, None, req_title)
+        year_str = re.match(r'\S*\s(\w+)-.*', req_title).group(1) 
+        return PrereqTree(MIN_YR_STNDG, [], WORD_TO_RANK[year_str], None)
 
     children = reqs_tree.find('ul')
     if not children:
@@ -106,9 +117,10 @@ def parse_reqs_rec(reqs_tree: BeautifulSoup) -> PrereqTree:
             req_title = next(title_strings)
             # skip over 'with a minimum grade of'
             next(title_strings)
-            min_grade, _ = next(title_strings).split()
+            # here, next(title_strings) is something like 'B (73%)'
+            min_grade = next(title_strings).split()[0]
 
-        return PrereqTree(-1, [req_title], min_grade, notes)
+        return PrereqTree(SINGLE_COURSE, [req_title], min_grade, notes)
 
     # for lines like 'Earn a minimum grade of /C+/ in each of the following:'
     if 'minimum grade' in req_title:
@@ -120,7 +132,7 @@ def parse_reqs_rec(reqs_tree: BeautifulSoup) -> PrereqTree:
     # but the entire 'Complete all of the following:' strings is together as
     # one string
     req_num = int(next(title_strings)) if ('all' not in req_title and 'each' not in req_title) \
-                                       else 0
+                                       else ALL
     results = [parse_reqs_rec(child) for child in children.contents]
     
     return PrereqTree(req_num, results, min_grade, notes)
@@ -139,9 +151,11 @@ if __name__ == '__main__':
     # CASE 5: complicated coreqs
     # url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/HytcJuaQV?q=CSC%20361&&%20%20%20%20limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Computer%20Communications%20and%20Networks&bcIt%20%20%20%20emType=courses'
     # CASE 6: min grade requirements
-    # Math100
-    url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/ByxQ12d6QE'
-    #url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/r1rPrjq_t?q=CSC%20361'
+    #url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/ByxQ12d6QE'
+    # CASE 7: AWR
+    # url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/r1rPrjq_t?q=CSC%20361'
+    # CASE 8: min year standing
+    url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/HyeHjkO674'
 
     # TODO: remove for loop below once implementation complete
     
