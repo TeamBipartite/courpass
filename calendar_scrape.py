c1cb0cb2877f7fa4fe503ffd4527df986cd5dd70
Johnathan Warawa <johnnyw@johnnyw.ca>
2023-10-14 18:35:52 -0700

    WEBSCRAPE_0013 inital implementation of min grade req parsing + some other edge cases

--- calendar_scrape.py
+++ calendar_scrape.py
@@ -14,10 +14,13 @@ numReqd is an integer representing the number of items from the reqs_list are
         - 0 represents 'ALL'
         - -1 is used to indidate a single course entry itself
         - -2 is used to indicate 'or permission from the department'  
-reqs_list is a list of PrereqTrees representing the items that are requied. If
-          num_reqd == -1, then this is a single course (Course object)
+reqs_list is a list of PrereqTrees representing the items that are required. If
+          num_reqd == -1, then this is a single course
+min_grade is a string representing a minimum grade needed, as a letter grade
+notes is a string giving space for any other special conditions on the req
+    - Currently this is only used to specify minimum year standing requirements 
 '''          
-PrereqTree = namedtuple('PrereqTree', ['num_reqd', 'reqs_list'])
+PrereqTree = namedtuple('PrereqTree', ['num_reqd', 'reqs_list', 'min_grade', 'notes'])
 
 WEBDRIVERS  = {webdriver.ChromeOptions: webdriver.Chrome, 
                webdriver.EdgeOptions: webdriver.Edge,
@@ -73,7 +76,7 @@ def parse_reqs(raw_html: str) -> PrereqTree:
     roots = [child_tag.find('li') if child_tag.name != 'li' else child_tag for child_tag in list_tree.contents] if list_tree else []
 
     # always 'complete one of' for the top-level items?
-    return (1, [parse_reqs_rec(root) for root in roots])
+    return PrereqTree(1, [parse_reqs_rec(root) for root in roots], None, None)
 
 def parse_reqs_rec(reqs_tree: BeautifulSoup) -> PrereqTree:
     '''
@@ -84,35 +87,61 @@ def parse_reqs_rec(reqs_tree: BeautifulSoup) -> PrereqTree:
     # needs to be saved to its own variable as we may call next() on it again
     title_strings = reqs_tree.strings
     req_title = next(title_strings)
+    min_grade = None
+    notes     = None
 
-    if req_title == 'or permission of the department.': return (-2, None)
+    # special cases that do not follow any other format
+    if req_title == 'or permission of the department.': 
+        return PrereqTree(-2, None, None, None)
+    elif req_title == 'Academic Writing Requirement (AWR) satisfied':
+        return PrereqTree(0, ['AWR'], None, None)
+    elif '-year standing' in req_title: # for 'minimum xyz-year standing'
+        return PrereqTree(0, None, None, req_title)
 
     children = reqs_tree.find('ul')
-    if not children: return PrereqTree(-1, req_title)
+    if not children:
+        # for lines like 
+        # 'Completed /Pre-Calculus 12/ with a minimum grade of /B (73%)'
+        if req_title == 'Completed ': 
+            req_title = next(title_strings)
+            # skip over 'with a minimum grade of'
+            next(title_strings)
+            min_grade, _ = next(title_strings).split()
+
+        return PrereqTree(-1, [req_title], min_grade, notes)
+
+    # for lines like 'Earn a minimum grade of /C+/ in each of the following:'
+    if 'minimum grade' in req_title:
+        min_grade = next(title_strings)
+        req_title = next(title_strings)
 
     # we can simply grab next(title_strings) here, because the cases where there
     # is a number, the number is in its own <span> (grabbed by the call to next),
     # but the entire 'Complete all of the following:' strings is together as
     # one string
-    req_num = int(next(title_strings)) if 'all' not in req_title else 0
-    pprint.pprint(children.contents)
+    req_num = int(next(title_strings)) if ('all' not in req_title and 'each' not in req_title) \
+                                       else 0
     results = [parse_reqs_rec(child) for child in children.contents]
     
-    return PrereqTree(req_num, results)
+    return PrereqTree(req_num, results, min_grade, notes)
 
 if __name__ == '__main__':
-    # TODO: remove mock urls
+    # TODO: incorporate into Jenkins regression (WEBSCRAPE_0014)
     # CASE 1: Pre and Coreqs present
-    # url = "https://www.uvic.ca/calendar/undergrad/index.php#/courses/ryzikO6QN?q=CSC%20361&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Computer%20Communications%20and%20Networks&bcItemType=courses"
+    #url = "https://www.uvic.ca/calendar/undergrad/index.php#/courses/ryzikO6QN?q=CSC%20361&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Computer%20Communications%20and%20Networks&bcItemType=courses"
     # CASE 2: No pre or coreqs
     #url = "https://www.uvic.ca/calendar/undergrad/index.php#/courses/HJZck_TmV?q=CSC105&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Computers%20and%20Information%20Processing&bcItemType=courses"
     # CASE 3: Prereqs only
     # url = "https://www.uvic.ca/calendar/undergrad/index.php#/courses/r1l00yY67E?q=SENG265&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Software%20Development%20Methods&bcItemType=courses"
     # CASE 4: more complicated trees
     #url ='https://www.uvic.ca/calendar/undergrad/index.php#/courses/Hkfbhda7E?q=SENG265&&%20%20%20%20limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Software%20Development%20Methods&bcItemType=cou%20%20%20%20rses'
-    url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/r1e06RP6XN'
+    #url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/r1e06RP6XN'
     # CASE 5: complicated coreqs
-    #url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/HytcJuaQV?q=CSC%20361&&%20%20%20%20limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Computer%20Communications%20and%20Networks&bcIt%20%20%20%20emType=courses'
+    # url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/HytcJuaQV?q=CSC%20361&&%20%20%20%20limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Computer%20Communications%20and%20Networks&bcIt%20%20%20%20emType=courses'
+    # CASE 6: min grade requirements
+    # Math100
+    url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/ByxQ12d6QE'
+    #url = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses/r1rPrjq_t?q=CSC%20361'
 
     # TODO: remove for loop below once implementation complete
     
