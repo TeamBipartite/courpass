46aa83756db00289fa2a02ce303a32a42bae72c1
ecamartins <65371150+ecamartins@users.noreply.github.com>
2023-10-22 10:47:55 -0700

    chore: import course module into views.py

--- academic_calendar_scraper/course_planning_tool/views.py
+++ academic_calendar_scraper/course_planning_tool/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from django.template.context_processors import csrf
+from .utils.course import Course
 
 @csrf_protect
 def course_planning_tool(request):
     c = {}
     c.update(csrf(request))
 
-    courses = ["CSC225", "MATH222"]
+    course1 = Course("CSC", 225, "Algorithms and Data Structures I", {}, {}, "https://www.uvic.ca/calendar/undergrad/index.php#/courses/Bk_cy_a7V" )
+    course2 = Course("MATH", 222, "Discrete Math", {}, {}, "https://www.uvic.ca/calendar/undergrad/index.php#/courses/Sye1xndp74" )
+    
+    courses = ["MATH 222", "CSC 226"]
+    """
+    TODO:
+    Use this list of Course objects once a custom tag is created
+    courses = [course1, course2]
+    """
 
     if request.method == "POST":
         c['searched'] = request.POST["prereq-search"]
