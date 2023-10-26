2f513b1dd09a3e9de887d1c3793e98525bee3b73
Johnathan Warawa <johnnyw@johnnyw.ca>
2023-10-25 20:35:24 -0700

    WEBSCRAPE_0014 replace empty dicts with null objects

--- tests/test_calendar_scrape.py
+++ tests/test_calendar_scrape.py
@@ -8,36 +8,36 @@ from prereqtree import PrereqTree
 class TestCalendarScrape:
 
 
-    CSC115_COURSE  = Course('CSC', '115', 'Fundamentals of Programming II', {}, {},
+    CSC115_COURSE  = Course('CSC', '115', 'Fundamentals of Programming II', None, None,
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5cbdf4e567a5c324003b0bb6' )
-    CSC116_COURSE  = Course('CSC', '116', 'Fundamentals of Programming II with Engineering Applications', {}, {},
+    CSC116_COURSE  = Course('CSC', '116', 'Fundamentals of Programming II with Engineering Applications', None, None,
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5cbdf4e667a5c324003b0bb9')
-    CSC225_COURSE  = Course('CSC', '225', 'Algorithms and Data Structures I', {}, {}, 
+    CSC225_COURSE  = Course('CSC', '225', 'Algorithms and Data Structures I', None, None, 
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5cbdf4ea67a5c324003b0bbd')
-    CSC226_COURSE  = Course('CSC', '226', 'Algorithms and Data Structures II', {}, {}, 
+    CSC226_COURSE  = Course('CSC', '226', 'Algorithms and Data Structures II', None, None, 
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/6307b1bdfe51a12031b36bfa')
-    CSC230_COURSE  = Course('CSC', '230', 'Introduction to Computer Architecture', {}, {}, 
+    CSC230_COURSE  = Course('CSC', '230', 'Introduction to Computer Architecture', None, None, 
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5cbdf4ec67a5c324003b0bc3')
-    CENG255_COURSE = Course('CENG', '255', 'Introduction to Computer Architecture', {}, {},
+    CENG255_COURSE = Course('CENG', '255', 'Introduction to Computer Architecture', None, None,
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5c4feeb019654524003846d1')
-    CHEM11_COURSE  = Course('unknown', 'Chemistry 11', '', {}, {}, '')
-    ECE255_COURSE  = Course('ECE', '255', 'Introduction to Computer Architecture', {}, {}, 
+    CHEM11_COURSE  = Course('unknown', 'Chemistry 11', '', None, None, '')
+    ECE255_COURSE  = Course('ECE', '255', 'Introduction to Computer Architecture', None, None, 
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5cbdf57704ce072400155f52')
-    GEOG226_COURSE = Course('GEOG', '226', 'Introduction to Quanitative Methods in Geography', {}, {}, 
+    GEOG226_COURSE = Course('GEOG', '226', 'Introduction to Quanitative Methods in Geography', None, None, 
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/6137a4830dbd1d32a146ff78')
-    MATH120_COURSE = Course('MATH', '120', 'Precalculus Mathematics', {}, {}, 
+    MATH120_COURSE = Course('MATH', '120', 'Precalculus Mathematics', None, None, 
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/63168d5cbee7272a8be640b9')
-    PRECALC12_COURSE=Course('unknown', 'Pre-Calculus 12', '', {}, {}, '')
-    PRNCPLES12_COURSE=Course('unknown', 'Principles of Mathematics 12', '', {}, {}, '')
-    SENG265_COURSE = Course('SENG', '265', 'Software Development Methods', {}, {}, 
+    PRECALC12_COURSE=Course('unknown', 'Pre-Calculus 12', '', None, None, '')
+    PRNCPLES12_COURSE=Course('unknown', 'Principles of Mathematics 12', '', None, None, '')
+    SENG265_COURSE = Course('SENG', '265', 'Software Development Methods', None, None, 
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5cbdf65404ce072400156009')
-    PSYC300A_COURSE= Course('PSYC', '300A', 'Statistical Methods in Psychology', {}, {},
+    PSYC300A_COURSE= Course('PSYC', '300A', 'Statistical Methods in Psychology', None, None,
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5d8d839e015c5b2400a72992')
-    STAT254_COURSE = Course('STAT', '254', 'Probability and Statistics for Engineers', {}, {},
+    STAT254_COURSE = Course('STAT', '254', 'Probability and Statistics for Engineers', None, None,
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5d1f6895fb68f3240022ccec')
-    STAT255_COURSE = Course('STAT', '255', 'Statistics for Life Sciences I', {}, {},
+    STAT255_COURSE = Course('STAT', '255', 'Statistics for Life Sciences I', None, None,
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5e615df5109f5025006793a7')
-    STAT260_COURSE = Course('STAT', '260', 'Introduction to Probability and Statistics I', {}, {},
+    STAT260_COURSE = Course('STAT', '260', 'Introduction to Probability and Statistics I', None, None,
                             'https://www.uvic.ca/calendar/undergrad/index.php#/courses/view/5e615e517eb8e0250078b7da')
                             
 
